d3962236730702ba530f5043fbf457a207881843
Darya Leonova <daryaleonova12@gmail.com>
2020-09-19 18:35:17 +0300

    ain_mainmdz_commit1

---
+++ main_main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+ /*       Scanner number = new Scanner(System.in);
+        int first, second, result;
+        System.out.println("Enter first number: ");
+        first = number.nextInt();
+        System.out.println("Enter second number: ");
+        second = number.nextInt();
+        result = first + second;
+        System.out.println("Your result is: " + result);
+*/
+/*
+        for (int i = 3; i <= 50; i+=3)
+            System.out.println(i);
+ */
+/*
+        for (int i = 9; i <= 50; i+=9)
+            System.out.println(i);
+*/
+/*
+        for (int i = 7; i <= 50; i+=7)
+            System.out.println(i);
+        for (int i = 5; i <= 50; i+=5)
+            System.out.println(i);
+*/
+/*        System.out.println("Введите ваш пароль и нажмите Enter:");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        if(name.equals("12345678")){
+            System.out.println("Доступ разрешен, добро пожаловать");
+        }
+        else
+        {
+            System.out.println("Пароль неверный, попробуйте ещё раз");
+        }*/
+/*
+        for (int i = 0; i<=15; i++){
+            System.out.println(Math.ceil(Math.random()*100));
+        }
+*/
+
+/*
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Я рада тебя видеть, " + name);
+*/
+
+/*
+        for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.println("Reverse: " + args[i]);
+*/
+        /*
+        int i = 0;
+        while (i < 50) {
+            if (i % 2 == 1) {
+                System.out.println(" i = " + i);
+            }
+            i++;
+        }
+ */
+/*
+        int i = 0;
+        while (i < 50) {
+            if (i % 2 == 0) {
+                System.out.println(" i = " + i);
+            }
+            i++;
+        }
+ */
+    }
+}
